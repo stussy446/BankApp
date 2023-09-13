b403bb7001fc7a958cdbea438ee9c6e2401c97b0
Steve Rector <steverector27@gmail.com>
2023-09-12 23:25:26 -0700

    Adds dynamic diplaying of withdrawals and deposits

--- starter/script.js
+++ starter/script.js
@@ -61,8 +61,96 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (movement, i) {
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${movement}₤</div>
+    </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, cur) => (acc += cur), 0);
+
+  labelBalance.textContent = `${balance} EURO`;
+};
+
+displayMovements(account1.movements);
+
+const user = 'Steven Thomas Williams'; // stw
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+const calcDisplaySummary = function (movements) {
+  const depositTotal = movements
+    .filter(movement => movement > 0)
+    .reduce((acc, cur) => (acc += cur), 0);
+
+  const withdrawalTotal = movements
+    .filter(movement => movement < 0)
+    .reduce((acc, cur) => (acc += cur), 0);
+
+  labelSumIn.textContent = `${depositTotal}₤`;
+  labelSumOut.textContent = `${withdrawalTotal}₤`;
+
+  const interest = movements
+    .filter(movement => movement > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter(interest => interest >= 1)
+    .reduce((acc, int) => (acc += int));
+
+  labelSumInterest.innerHTML = `${interest}₤`;
+};
+
+createUsernames(accounts);
+calcDisplaySummary(account1.movements);
+
+calcDisplayBalance(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
 /////////////////////////////////////////////////
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
